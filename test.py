736d93d9ff5c365d6de49210628db32bd4506e8d
joegaglian <joseph.gagliano@bell.ca>
2021-07-20 14:26:46 +0000

    copying local files with python

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+mylist = []
+username = input("Name")
+userage = input("age")
+userheight = input("height")
+mylist.append(username)
+mylist.append(userage)
+mylist.append(userheight)
+print(mylist)
